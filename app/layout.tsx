ce760b733295e0819142df0e3fe46870e1edea14
Andrew T <andrewtirtokusumo@gmail.com>
2024-10-14 14:30:37 +0700

    update layout gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import localFont from 'next/font/local'
-import { Header } from '@/components/Header'
-import { Footer } from '@/components/Footer'
-import { ThemeProvider } from '@/components/theme-provider'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+import { ThemeProvider } from '../components/theme-provider'
+import Script from 'next/script'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -21,6 +22,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-7VT6FZNSK9`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7VT6FZNSK9');
+          `}
+        </Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
